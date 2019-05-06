69a99c306949e487cba41478cef50fd6c65fbcff
phtb233 <stephen_621@msn.com>
2019-05-06 19:20:43 +0100

    added menu buttons

--- src/Main.java
+++ src/Main.java
@@ -1,114 +1,145 @@
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+import javax.media.*;
 import javax.swing.*;
-import javax.media.Player;
-import javax.media.Manager;
-import javax.media.NoPlayerException;
-import javax.media.ControllerListener;
-import javax.media.ControllerEvent;
-import javax.media.RealizeCompleteEvent;
-import javax.media.PrefetchCompleteEvent;
-import javax.media.EndOfMediaEvent;
-import java.awt.Component;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main {
-	public static void main(String az[]) {
+	public static void main(String[] args) {
         new QAMediaPlayer();
 	}
 }
 
-class QAMediaPlayer {
-    public static final short WINDOW_WIDTH = 800;
-    public static final short WINDOW_HEIGHT = 600;
-    public static final short PLAYER_WIDTH = 640;
-    public static final short PLAYER_HEIGHT = 480;
-    JFrame jframe;
-    JDesktopPane jdp;
-    PlayerFrame pf;
-    public QAMediaPlayer(){
+/**
+ * A simple video player utilizing javax.media.Player.
+ * Use the menu bar to open a video file.
+ * BUG: Sometimes the video doesn't appear, no exceptions/warnings
+ */
+class QAMediaPlayer implements ActionListener, ControllerListener {
+    private static final short WINDOW_WIDTH = 800;
+    private static final short WINDOW_HEIGHT = 600;
+    private JFrame jframe;
+    private JMenu file;
+    private JMenuItem open;
+    private JMenuItem exit;
+    private Player player;
+    private Component videoFrame;
+    private Component playerPlaybackControls;
+    private JPanel playbackPanel;
+    private JButton playBtn;
+    private JButton stopBtn;
+    QAMediaPlayer(){
         jframe = new JFrame("QAMediaPlayer");
-        jdp = new JDesktopPane();
-        pf = new PlayerFrame();
-        jframe.add(jdp);
-        jdp.add(pf);
+        JMenuBar menuBar = new JMenuBar();
+        open = new JMenuItem("Open");
+        exit = new JMenuItem("Exit");
+        file = new JMenu("File");
+        menuBar.add(file);
+        file.add(open);
+        file.add(exit);
+        jframe.setJMenuBar(menuBar);
+        open.addActionListener(this);
+        exit.addActionListener(this);
+        playBtn = new JButton("Play");
+        stopBtn = new JButton("Stop");
+        playbackPanel = new JPanel();
+        playbackPanel.setLayout(new FlowLayout());
+        playbackPanel.add(playBtn);
+        playbackPanel.add(stopBtn);
         jframe.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setVisible(true);
-        pf.setLocation(WINDOW_WIDTH / 2 - PLAYER_WIDTH / 2, 
-                WINDOW_HEIGHT / 2 - PLAYER_HEIGHT / 2);
-        pf.setVisible(true);
-
     }
-}
 
-class PlayerFrame extends JInternalFrame implements ControllerListener {
-    // Had to do this as JInternalFrame is a serializable type.
-    // Warnings otherwise.
-    private static final long serialVersionUID = 42L;
-    private Player player;
-    private Component visual;
-    private Component control;
-    private int vid_width, vid_height, cont_height;
-    public PlayerFrame(){
-        super("", false, false, false, false);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == open){
+            // show the sample files that will definitely work with the player
+            JFileChooser jFileChooser = new JFileChooser("./media");
+            int returnVal = jFileChooser.showOpenDialog(jframe);
+            if(returnVal == JFileChooser.APPROVE_OPTION){
+                playVideo(jFileChooser.getSelectedFile());
+            }
+        } else if(e.getSource() == exit){
+            System.exit(0);
+        }
+    }
+
+    private void playVideo(File selectedFile) {
+        if(player != null){
+            destroyPlayer();
+            createPlayer(selectedFile);
+        } else {
+            createPlayer(selectedFile);
+        }
+    }
+
+    /**
+     * Remove the current player instance and associated playback components.
+     * Would be wiser to check whether the same file has been passed to the player, in that case just replay the video.
+     */
+    private void destroyPlayer(){
+        player.stop();
+
+        for(Component c : jframe.getContentPane().getComponents()){
+            if(c == playbackPanel){
+                jframe.remove(c);
+            }
+            if(c == playerPlaybackControls){
+                jframe.remove(playerPlaybackControls);
+                playerPlaybackControls = null;
+            }
+            if(c == videoFrame){
+                jframe.remove(videoFrame);
+                videoFrame = null;
+            }
+        }
+        player = null;
+    }
+
+    private void createPlayer(File selectedFile){
         try {
-            File vid = new File("../media/another_movie_2.mov"); 
-            String vid_url = "file://" + vid.getCanonicalPath();
-            this.setTitle(vid_url);
-            URL url = new URL(vid_url);
+            jframe.setTitle("QAMediaPlayer: " + selectedFile.getName());
+            URL url = new URL("file:///" + selectedFile.getCanonicalPath());
             player = Manager.createPlayer(url);
-            // Have to wait for realize event to complete.
             player.addControllerListener(this);
             player.realize();
-            //this.add(visual);
-        } catch (MalformedURLException mue){
-            System.out.println(mue.toString());
-        } catch(IOException ioe){
-            System.out.println(ioe.toString());
-        } catch(NoPlayerException npe){
-            System.out.println(npe.toString());
-        } catch(Exception e){
+        } catch(IOException | NoPlayerException e){
             System.out.println(e.toString());
         }
     }
 
-    public void controllerUpdate(ControllerEvent ce){
-        if(ce instanceof RealizeCompleteEvent){
-            System.out.println("Realize Completed");
-            // Initiate prefetch
-            player.prefetch();
-        } else if (ce instanceof PrefetchCompleteEvent) {
-            // Can prepare video for playback
-            System.out.println("Prefetch Completed");
-            // Get the video file width and height and set this frame
-            // to the same dimensions. Also consider the control UI panel.
-            if ((visual = player.getVisualComponent()) != null) {
-                vid_width = visual.getPreferredSize().width;
-                vid_height = visual.getPreferredSize().height;
-                add(visual, BorderLayout.CENTER);
-            } else vid_width = QAMediaPlayer.PLAYER_WIDTH;
-            if ((control = player.getControlPanelComponent()) != null){
-                cont_height = control.getPreferredSize().height;
-                control.setLocation(QAMediaPlayer.PLAYER_WIDTH / 2, 
-                        QAMediaPlayer.PLAYER_HEIGHT - cont_height);
-                add(control, BorderLayout.SOUTH);
+    @Override
+    public void controllerUpdate(ControllerEvent controllerEvent) {
+        if(controllerEvent instanceof RealizeCompleteEvent) player.prefetch();
+        else if(controllerEvent instanceof PrefetchCompleteEvent){
+            if((videoFrame = player.getVisualComponent()) != null){
+                Dimension playerPreferredSize = new Dimension();
+                if((playerPlaybackControls = player.getControlPanelComponent()) != null){
+                    // Use the player's built in playback controls
+                    playerPreferredSize.setSize(videoFrame.getPreferredSize().getWidth(),
+                            playerPlaybackControls.getPreferredSize().getHeight() +
+                                    videoFrame.getPreferredSize().getHeight());
+                    jframe.add(playerPlaybackControls, BorderLayout.SOUTH);
+                } else {
+                    // Create and add default playback controls
+                    playerPreferredSize.setSize(videoFrame.getPreferredSize().getWidth(),
+                            50 + videoFrame.getPreferredSize().getHeight());
+                    jframe.add(playbackPanel, BorderLayout.SOUTH);
+                }
+                jframe.setSize(playerPreferredSize);
+                jframe.add(videoFrame, BorderLayout.CENTER);
+                jframe.validate();
+                player.start();
             }
-            // The video is stretched to fit the parent pane dimensions.
-            //setSize(vid_width, vid_height + cont_height);
-            setSize(QAMediaPlayer.PLAYER_WIDTH, 
-                    QAMediaPlayer.PLAYER_HEIGHT);
-            // Not sure what this does...
-            validate();
-            // Start the video.
-            player.start();
-        } else if (ce instanceof EndOfMediaEvent){
-            System.out.println("Video Finished");
-        }
+        } else if(controllerEvent instanceof EndOfMediaEvent)
+            System.out.println("Video finished");
+
     }
 }
 // realize?
